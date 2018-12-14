fb244075774f6147a600ec41b5d19ecd6b14dce8
Mihai Nita <mnita@google.com>
2018-12-14 08:54:41 -0800

    Split into separate components for separate loggers

--- color-logger-jdk/src/test/java/ColorBaseTest.java
+++ color-logger-jdk/src/test/java/ColorBaseTest.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import org.junit.runner.JUnitCore;
 
 public class ColorBaseTest {
-    static boolean waitForEnter = false;
 
     public static String getTargetDir() {
         URL url = ColorBaseTest.class.getResource(".");
@@ -20,22 +19,9 @@ public class ColorBaseTest {
         return ".";
     }
 
-    static void pressEnter() {
-        if (waitForEnter) {
-            System.out.println("Press ENTER to continue...");
-            Scanner scan = new Scanner(System.in);
-            scan.nextLine();
-            scan.close();
-        }
-    }
-
     public static void main(String[] argv) {
-        waitForEnter = true;
         Class<?> [] classesArray = {
             ColorJdkTest.class,
-            ColorLog4jTest.class,
-            ColorLog4jXmlTest.class,
-            ColorLogbackTest.class
         };
         JUnitCore.runClasses(classesArray);
     }
---
+++ color-logger-log4j/src/test/java/ColorBaseTest.java
@@ -0,0 +1,29 @@
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.junit.runner.JUnitCore;
+
+public class ColorBaseTest {
+
+    public static String getTargetDir() {
+        URL url = ColorBaseTest.class.getResource(".");
+        if (null == url)
+            return ".";
+
+        try {
+            return url.toURI().getPath();
+        } catch (URISyntaxException e) {
+        }
+
+        return ".";
+    }
+
+    public static void main(String[] argv) {
+        Class<?> [] classesArray = {
+            ColorLog4jTest.class,
+            ColorLog4jXmlTest.class,
+        };
+        JUnitCore.runClasses(classesArray);
+    }
+}
